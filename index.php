a251668444f1e6ec515b4484606cc3218dc7f334
MD Tasin Rahman <tasintt@gmail.com>
2023-04-05 10:20:55 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>LOGIN</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="box">
+		<form action="login.php" method="post">
+			<center><img src="../spms/img/logo.png" width="140" height="112"></center>
+			<h2>Sign in</h2>
+			<input type="text" name = "id" id="userField" placeholder="User ID" required="required">
+			<input type="password" name = "pass" id="Password" placeholder="Password" >
+			<div class="links">
+				<a href="#" class="forgot-pass">Forgot Password ?</a>
+
+
+
+
+			</div>
+			<input type="submit" id="login" value="Login">
+		</form>
+	</div>
+	<script src="/js/index.js"></script>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,81 @@
+<?php
+include "connect.php" ;
+session_start();
+if (isset($_POST['id']) && isset($_POST['pass'])) {
+    function validate($data){
+        $data = trim($data);
+        $data = stripcslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $id = validate($_POST['id']);
+    $pass = validate($_POST['pass']);
+
+    // if (empty($id)){
+    //     header("Location: index.php?error= Enter ID");
+    //     exit();
+
+    // } elseif (empty($pass)) {
+    //     header("Location: index.php?error= Enter Password");
+    //     exit();
+    // } else{
+    if ( strlen(strval($id)) === 4){ 
+        $sql = "SELECT * from employee_t where employeeID='$id' and password='$pass'";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_assoc($result);
+        if (mysqli_num_rows($result) === 1) {
+            if ($row['employeeID'] === $id && $row['password'] === $pass) {
+                //echo "hello " . $row['name'] ;
+                $_SESSION['name'] = $row['firstName'];
+                $_SESSION['id'] = $row['employeeID'];
+                header("Location: F_Dashboard.php");
+                exit();
+            } 
+        } else {
+            header("Location: index.php?error = Incorrect ID or Password");
+            exit();
+        }
+    } elseif ( strlen(strval($id)) === 7 ){
+        $sql = "SELECT * from student_t where studentID='$id' and password='$pass'";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_assoc($result);
+        if (mysqli_num_rows($result) === 1) {
+            if ($row['studentID'] === $id && $row['password'] === $pass) {
+                //echo "hello " . $row['name'] ;
+                $_SESSION['name'] = $row['firstName'] ." " . $row['lastName'];
+                $_SESSION['id'] = $row['studentID'];
+                header("Location: S_Dashboard.php");
+                exit();
+            } 
+        } else {
+            header("Location: index.php?error = Incorrect ID or Password");
+            exit();
+        }
+
+    } elseif ( strlen(strval($id)) === 5 ) { 
+        $sql = "SELECT * from authority_t where authorityID='$id' and password='$pass'";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_assoc($result);
+        if (mysqli_num_rows($result) === 1) {
+            if ($row['authorityID'] === $id && $row['password'] === $pass) {
+                //echo "hello " . $row['name'] ;
+                $_SESSION['name'] = $row['firstName'] ." " . $row['lastName'];
+                $_SESSION['id'] = $row['authorityID'];
+                header("Location: H_Dashboard.php");
+                exit();
+            } 
+        } else {
+            header("Location: index.php?error = Incorrect ID or Password");
+            exit();
+        }
+    } else {
+        header("Location: index.php?error");
+        exit();
+    }
+        // sesh bhai
+    //}
+} else {
+    header("Location: index.php?error");
+    exit();
+}
